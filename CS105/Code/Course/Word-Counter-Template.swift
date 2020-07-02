0f6c0400d9bb64ac6374626addbfee4f8863a60a
Nathan Mull <nmull@uchicago.edu>
2020-07-02 08:04:17 -0500

    added lab 2 and notes on ssh

---
+++ CS105/Code/Course/Word-Counter-Template.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func main() {
+    // get the input from standard input using calls to readLine
+    // convert the input String into a [String] of words
+    // convert the [String] of words into a [String: Int] dictionary with words and frequencies
+    // convert the dictionary into an array of (String, Int) pairs, sorted by decreasing value of the Int part.
+    // print the values in the sorted array with the right format.
+}
+
+// main()
