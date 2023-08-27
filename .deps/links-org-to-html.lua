bde5ab98160c740e04ec1a4ca019ad63c62d74af
Nathan Mull <nmmull@gmail.com>
2023-08-27 00:41:48 -0400

    updated links for schedule

--- .deps/links-org-to-html.lua
+++ .deps/links-org-to-html.lua
@@ -2,6 +2,6 @@ function Link(el)
   if not (el.target:sub(1, 7) == "http://" or el.target:sub(1, 8) == "https://") then
      el.target = string.gsub(el.target, "%.org", ".html")
   end
-  el.target = string.gsub(el.target, "::", "#")
+  el.target = string.gsub(el.target, "::", "")
   return el
 end
