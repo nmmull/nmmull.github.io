894d5948a836d07ed7ce4787f3ea2c07a23d740f
Nathan  Mull <NathanM@csadmins-MacBook-Pro.local>
2023-11-03 10:05:29 -0400

    added hw08

---
+++ CS132-F23/Assignments/08-Homework/hw08prog.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+part_a = None #TODO
+
+part_b = None #TODO
+
+part_c = None #TODO
+
+part_d = None #TODO
+
+part_e = None #TODO
+
+part_f = None #TODO
+
+part_g = None #TODO
+
+part_h = None #TODO
+
+part_i = None #TODO
+
+part_j = None #TODO
+
+part_k = None #TODO
+
+part_l = None #TODO
+
+part_m = None #TODO
