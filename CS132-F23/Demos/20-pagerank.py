96499838e8e343c4a9bdfe63cc2d0e5d5ce01291
Nathan  Mull <NathanM@csadmins-MacBook-Pro.local>
2023-11-18 15:46:01 -0500

    hw10

--- CS132-F23/Demos/20-pagerank.py
+++ CS132-F23/Demos/20-pagerank.py
@@ -8,8 +8,19 @@ a = np.array([
     [1/60, 1/6, 19/60, 7/15, 1/60,  1/60],
     [1/60, 1/6,  1/60, 7/15, 7/15,  1/60]])
 
-print(a)
 
 # step 4: compute eigenvector
+eigenvalues, eigenvectors = np.linalg.eig(a)
+
+
+steady_state = np.real(eigenvectors)[:,0]
+steady_state /= np.sum(steady_state)
+
+print()
+print(steady_state)
 
 # step 5: display indice (pages) in order of rank
+page_rank = np.argsort(steady_state)[::-1] + 1
+
+print()
+print(page_rank)
